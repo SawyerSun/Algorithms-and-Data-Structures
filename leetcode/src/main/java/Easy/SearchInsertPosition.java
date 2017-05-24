32c5deec82d31a8e975f22dfb5cd3a964e233cc6
sawyer <sawyersunyue@gmail.com>
2017-05-24 21:36:36 +0800

    完成Leetcode 35. Search Insert Position

---
+++ leetcode/src/main/java/Easy/SearchInsertPosition.java
@@ -0,0 +1,37 @@
+package Easy;
+
+/**
+ * Given a sorted array and a target value, return the index if the target is found.
+ * If not, return the index where it would be if it were inserted in order.
+ * <p>
+ * You may assume no duplicates in the array.
+ * <p>
+ * Here are few examples.
+ * [1,3,5,6], 5 → 2
+ * [1,3,5,6], 2 → 1
+ * [1,3,5,6], 7 → 4
+ * [1,3,5,6], 0 → 0
+ * <p>
+ * Tags: Array, Binary Search
+ *
+ * @author sunyue
+ * @version 1.0    2017/5/24 21:23
+ */
+public class SearchInsertPosition {
+    /**
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     * Leetcode运行时间：6 ms
+     */
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) high = mid - 1;
+            else low = mid + 1;
+        }
+        return low;
+    }
+}
